041717337e038b57975101a84b9de2a04af3dbaa
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2022-06-01 01:46:30 +0600

    Добавил компонент AddTaskForm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import AddTaskForm from "./Components/AddTaskForm";
 
 const App = () => {
-
+    return (
+        <AddTaskForm />
+    );
 }
 
 export default App;
---
+++ src/Components/AddTaskForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AddTaskForm.css';
+
+const AddTaskForm = () => {
+    return (
+        <div className='form-body'>
+            <form>
+                <input className='input' type='text' placeholder='Add new task' />
+                <button className='btn' type='submit'>Add</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddTaskForm;
